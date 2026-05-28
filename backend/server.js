884083399222a4d6e5f3cf737f678294e8e35a21
Prasad Mandapati <prasad.mandapati91@gmail.com>
2026-05-28 19:42:29 +0530

    backend setup completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
